a7b472ecbc028544e229e1479b5c9a9fc5265447
Christopher Persinet <christopherpersinet@gmail.com>
2020-12-14 13:13:11 +0100

    feat: affichage de la map avec leaflet plus marqueur

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,16 @@ import '../styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+console.log("test")
+var mymap = L.map('mapid').setView([49.25, 4.0333], 15);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+var marker = L.marker([49.2257934073045, 4.01539227165092]).addTo(mymap);
