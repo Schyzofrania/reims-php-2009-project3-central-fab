38fcfacf8fc44e5fa974f71c96ca096e65fc7633
Charlie <github@zipmail.pw>
2020-12-16 17:38:49 +0100

    Added address markers on LeafLet map

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -5,16 +5,44 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\FablabRepository;
+use App\Entity\Fablab;
+use maxh\Nominatim\Nominatim;
 
+/**
+ * @Route("/map")
+*/
 class MapController extends AbstractController
 {
     /**
-     * @Route("/map", name="map")
+     * @Route("/", name="map")
      */
     public function index(): Response
     {
+        $FablabRepository = $this->getDoctrine()
+             ->getRepository(Fablab::class)
+             ->findAll();
+
+        if (!$FablabRepository) {
+            throw $this->createNotFoundException(
+                'No Fablab found in Falab\'s table.'
+            );
+        }
+
+        $url = 'http://nominatim.openstreetmap.org/';
+        $nominatim = new Nominatim($url);
+
+        $data = [];
+        for($i = 0; $i < count($FablabRepository); $i++) {
+            $search = $nominatim->newSearch();
+            $mapAddress = $search->query($FablabRepository[$i]->getAdress());
+            $data[] = $nominatim->find($mapAddress);
+        }
+
         return $this->render('map/index.html.twig', [
-            'controller_name' => 'MapController',
+            'cities' => $FablabRepository,
+            'maps' => $data,
         ]);
     }
 }
