35f755a70e2678893d34fb20055b99af27617054
Charlie <github@zipmail.pw>
2021-01-26 15:29:09 +0100

    add alert email

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\ChangeEmailType;
 use App\Form\ChangePasswordType;
+use Symfony\Component\Form\FormError;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,28 +22,51 @@ class ProfileController extends AbstractController
         Request $request,
         UserPasswordEncoderInterface $passwordEncoder,
         EntityManagerInterface $entityManager
+        // ,
+        // User $user
     ): Response {
         $user = $this->getUser();
 
         $formEmail = $this->createForm(ChangeEmailType::class, $user);
         $formEmail->handleRequest($request);
 
+        if ($formEmail->isSubmitted() && $formEmail->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre courriel à bien été changé !');
+        }
+
+
         $formPassword = $this->createForm(ChangePasswordType::class, $user);
         $formPassword->handleRequest($request);
 
-        if ($formEmail->isSubmitted() && $formEmail->isValid()) {
-            $entityManager->flush();
-        }
-
         if ($formPassword->isSubmitted() && $formPassword->isValid()) {
+            if ($passwordEncoder->isPasswordValid($user, $formPassword->get('oldPassword')->getData())) {
+                $user->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $user,
+                        $formPassword->get('plainPassword')->getData()
+                    )
+                );
+                $entityManager->flush();
+
+                $this->addFlash('notice', 'Votre mot de passe à bien été changé !');
+
+                return $this->redirectToRoute('user_profile', ['slug' => $user->getSlug()]);
+            } else {
+                $formPassword->addError(new FormError('Ancien mot de passe incorrect'));
+            }
+
+
+/*
             $user->setPassword(
                 $passwordEncoder->encodePassword(
                     $user,
                     $formPassword->get('plainPassword')->getData()
                 )
             );
-
-            $entityManager->flush();
+*/
+            // $entityManager->flush();
         }
 
         return $this->render('profile/index.html.twig', [
